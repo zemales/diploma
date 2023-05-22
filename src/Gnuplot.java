0885b5d67e6362522e7280ebf8a0825866271b74
zemales <ityurikovman@mail.ru>
2023-05-22 15:50:21 +0300

    Gnuplot.java modified for filenames

--- src/Gnuplot.java
+++ src/Gnuplot.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 public class Gnuplot {
-    public static void gnuplotCode(File targetFile) throws IOException {
-        PrintStream printStream = new PrintStream(new FileOutputStream(targetFile + ".plt"));
-        printStream.println("set terminal png");
-        printStream.println("set output \"" + targetFile.getName() + ".png\"");
-        printStream.println("plot \"" + targetFile.getName() + "\"");
-        printStream.close();
+    public static void gnuplotCode(File targetFile) {
+        try (PrintStream printStream = new PrintStream(new FileOutputStream( targetFile + ".plt"))) {
+            printStream.println("set terminal png");
+            printStream.println("set output \"PNG" + targetFile.getName().substring(0, targetFile.getName().length()-4) + ".png\"");
+            printStream.println("plot \"" + targetFile.getName() + "\"");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

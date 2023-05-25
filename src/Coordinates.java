c49c5d3bd136e7e73e1af2ff07204c62baf0b89e
zemales <ityurikovman@mail.ru>
2023-05-25 18:32:00 +0300

    3d ghaphic in gnuplot all of the rain gauges in Coordinates

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -6,11 +6,39 @@ public class Coordinates {
     private static final HashMap<Integer, Coordinates> coordinatesHashMap = new HashMap<>();
 
     static {
+        coordinatesHashMap.put(1001, new Coordinates(96, 96));
+        coordinatesHashMap.put(1002, new Coordinates(91, 95));
+        coordinatesHashMap.put(1003, new Coordinates(80, 94));
+        coordinatesHashMap.put(1004, new Coordinates(93, 88));
+        coordinatesHashMap.put(1005, new Coordinates(98, 92));
+        coordinatesHashMap.put(1006, new Coordinates(81 ,102));
+        coordinatesHashMap.put(1007, new Coordinates(85, 103));
+        coordinatesHashMap.put(1008, new Coordinates(91, 103));
+        coordinatesHashMap.put(1009, new Coordinates(91, 105));
+        coordinatesHashMap.put(1010, new Coordinates(116, 104));
+        coordinatesHashMap.put(1011, new Coordinates(104, 110));
+        coordinatesHashMap.put(1013, new Coordinates(101, 112));
+        coordinatesHashMap.put(1014, new Coordinates(102, 99));
+        coordinatesHashMap.put(1015, new Coordinates(99, 97));
+        coordinatesHashMap.put(1016, new Coordinates(91, 112));
+        coordinatesHashMap.put(1017, new Coordinates(95, 96));
+        coordinatesHashMap.put(1018, new Coordinates(80, 84));
+        coordinatesHashMap.put(1019, new Coordinates(86, 86));
+        coordinatesHashMap.put(1020, new Coordinates(87, 90));
+        coordinatesHashMap.put(1021, new Coordinates(92, 91));
         coordinatesHashMap.put(1022, new Coordinates(99, 95)); //pluvio
+        coordinatesHashMap.put(1023, new Coordinates(100, 94));
+        coordinatesHashMap.put(1024, new Coordinates(90, 103));
         coordinatesHashMap.put(1025, new Coordinates(94, 100)); //pluvio
         coordinatesHashMap.put(1026, new Coordinates(98, 100)); //pluvio
+        coordinatesHashMap.put(1027, new Coordinates(97, 104));
+        coordinatesHashMap.put(1028, new Coordinates(101, 103));
+        coordinatesHashMap.put(1029, new Coordinates(104, 105));
+        coordinatesHashMap.put(1030, new Coordinates(97, 108));
+        coordinatesHashMap.put(1031, new Coordinates(93, 106));
         coordinatesHashMap.put(1032, new Coordinates(88, 93)); //pluvio
         coordinatesHashMap.put(1034, new Coordinates(96, 97)); //pluvio
+        coordinatesHashMap.put(1035, new Coordinates(107, 98));
     }
 
     public Coordinates() {
--- src/Gnuplot.java
+++ src/Gnuplot.java
@@ -13,4 +13,19 @@ public class Gnuplot {
             e.printStackTrace();
         }
     }
+
+    public static void gnuplotStandardDeviation(File targetFile) {
+        try (PrintStream printStream = new PrintStream(new FileOutputStream( targetFile + ".plt"))) {
+            printStream.println("set xlabel \"X\"");
+            printStream.println("set ylabel \"Y\"");
+            printStream.println("set zlabel \"Standard Deviation\"");
+            printStream.println("set dgrid3d");
+            printStream.println("set view 45, 60, 1.0, 1.0");
+            printStream.println("set terminal png");
+            printStream.println("set output \"PNG" + targetFile.getName().substring(0, targetFile.getName().length()-4) + ".png\"");
+            printStream.println("splot \"" + targetFile.getName() + "\" using 1:2:3 with pm3d");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
